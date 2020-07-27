516d88dd92b143cf39a4aa69e1a47c15bece10db
MichaelLourenco <michael.lourenco@pixter.com.br>
2020-07-26 22:51:52 -0300

    Iniciando com React JS

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get("/",(request, response)=>{
-    return response.send("Hello World");
+    return response.json({
+        evento:"Semana PmniStack 11.0",
+        aluno:"Michael Lourenço"
+    })
 })
 
 app.listen(3333)
