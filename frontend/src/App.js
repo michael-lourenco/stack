1a5c3917093e12a32b41922a7fd016ab5854d684
MichaelLourenco <michael.lourenco@pixter.com.br>
2020-07-27 18:21:14 -0300

    "Criando layour Profile"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './global.css'
-import Logon from './pages/Logon'
+
+import Routes from './routes/routes'
+
 function App() {
 
   return (
-    <Logon/>
+    <Routes />
   );
 }
 
