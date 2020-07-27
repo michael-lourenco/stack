7298fceeafebfaaad10a3cf6b4f25f2f8ed5e0a2
MichaelLourenco <michael.lourenco@pixter.com.br>
2020-07-27 15:08:11 -0300

    "Base backend criada"

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('../routes/routes')
 
 const app = express()
 
+app.use(cors())
 //informar que usaremos JSON no corpo das requisições
 app.use(express.json())
 app.use(routes)
